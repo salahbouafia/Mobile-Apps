ce0952ee7466d76784cf71ae40915783a7460356
salah bouafia <khourti10@gmail.com>
2025-01-26 22:07:20 +0100

    tic tac toe By Aness

---
+++ tictactoe/app/src/main/java/com/aness/tictactoe/MainActivity.java
@@ -0,0 +1,98 @@
+package com.aness.tictactoe;
+
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean playerXTurn = true;
+    private Button[][] buttons = new Button[3][3];
+    private TextView textViewStatus;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewStatus = findViewById(R.id.textViewStatus);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                String buttonID = "button_" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+                buttons[i][j].setOnClickListener(this::onClick);
+            }
+        }
+
+        findViewById(R.id.buttonReset).setOnClickListener(v -> resetGame());
+    }
+
+    private void onClick(View v) {
+        Button button = (Button) v;
+        if (!button.getText().toString().equals("")) {
+            return;
+        }
+
+        button.setText(playerXTurn ? "X" : "O");
+
+        if (checkForWin()) {
+            textViewStatus.setText(playerXTurn ? "Player X Wins!" : "Player O Wins!");
+            disableButtons();
+        } else {
+            playerXTurn = !playerXTurn;
+            textViewStatus.setText(playerXTurn ? "Player X's turn" : "Player O's turn");
+        }
+    }
+
+    private boolean checkForWin() {
+        String[][] field = new String[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1]) && field[i][0].equals(field[i][2]) && !field[i][0].equals("")) {
+                return true;
+            }
+            if (field[0][i].equals(field[1][i]) && field[0][i].equals(field[2][i]) && !field[0][i].equals("")) {
+                return true;
+            }
+        }
+
+        if (field[0][0].equals(field[1][1]) && field[0][0].equals(field[2][2]) && !field[0][0].equals("")) {
+            return true;
+        }
+        if (field[0][2].equals(field[1][1]) && field[0][2].equals(field[2][0]) && !field[0][2].equals("")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void disableButtons() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setEnabled(false);
+            }
+        }
+    }
+
+    private void resetGame() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                buttons[i][j].setText("");
+                buttons[i][j].setEnabled(true);
+            }
+        }
+        playerXTurn = true;
+        textViewStatus.setText("Player X's turn");
+    }
+}
