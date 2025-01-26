f77925978e2de2abf9948f15e1606d4705d16e5d
Djasser <djasser.omeiri@etu.univ-batna2.dz>
2025-01-26 22:59:41 +0100

    EquationSolver interface by Djasser

--- EquationSolver/app/src/main/java/com/example/equationsolver/MainActivity.java
+++ EquationSolver/app/src/main/java/com/example/equationsolver/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mix_of_layouts);
+        setContentView(R.layout.constraintlayout);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
